0b0871bf0b000f69c25dc0c3d4df5ccd39b439f5
Andrew Cook <andrewjcook93@gmail.com>
2023-08-14 21:32:53 -0500

    styles: concert-preview, margins enlarged

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,11 @@ const Home = () => {
   return (
     <div>
       
-      <header className="heading">
+      {/* <header className="heading">
         <span className="heading__sub heading__sub--1">Krystal's </span>&nbsp;
         <span className="heading__sub heading__sub--2">Concert &nbsp;&nbsp; </span>
         <span className="heading__sub heading__sub--3">&nbsp; Scrapbook</span>
-      </header>
+      </header> */}
 
       <ConcertList />
     </div>
