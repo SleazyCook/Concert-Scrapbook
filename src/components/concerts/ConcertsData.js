9d230444dbbfa09ce855f6ac568da158867e8cd8
Andrew Cook <andrewjcook93@gmail.com>
2023-08-14 20:27:11 -0500

    add: artistPics

--- src/components/concerts/ConcertsData.js
+++ src/components/concerts/ConcertsData.js
@@ -9,7 +9,7 @@ let concertsData = [
     state: "Texas",
     venue: "Bayou Music Center",
     meet: true,
-    artistPic: "https://i.imgur.com/uhieA8I.png",
+    artistPic: "https://i.imgur.com/N51aAcP.png",
   },
   {
     id: 2,
@@ -21,7 +21,7 @@ let concertsData = [
     state: "Texas",
     venue: "Bayou Music Center",
     meet: true,
-    artistPic: "https://i.imgur.com/uhieA8I.png",
+    artistPic: "https://i.imgur.com/Ph3CaDP.png",
   },
   {
     id: 3,
@@ -38,6 +38,59 @@ let concertsData = [
     videos: [],
     wentWith: ["Drew", "Bria"],
     merch: []
+  },
+  {
+    id: 4,
+    artist: "Thee Sacred Souls",
+
+    artistPic: "https://i.imgur.com/EhHqmd3.png"
+  },
+  {
+    id: 5,
+    artist: "Dreamer Isioma",
+
+    artistPic: "https://i.imgur.com/hG59ZUZ.png"
+  },
+  {
+    id: 6,
+    artist: "Janet Jackson",
+
+    date: "June 2nd",
+
+    artistPic: "https://i.imgur.com/SFtVoZV.png"
+  },
+  {
+    id: 7,
+    artist: "Baby Rose",
+
+    date: "June 8th",
+
+    artistPic: "https://i.imgur.com/V3YxxEn.png"
+  },
+  {
+    id: 8,
+    artist: "Q",
+
+    date: "June 8th",
+
+    artistPic: "https://i.imgur.com/y58vvWx.png"
+  },
+
+  {
+    id: 9,
+    artist: "Erykah Badu",
+
+    date: "July, 23rd",
+
+    artistPic: "https://i.imgur.com/3iniLfc.png"
+  },
+  {
+    id: 10,
+    artist: "Snoop Dogg",
+
+    date: "August 14th",
+
+    artistPic: "https://i.imgur.com/Cm6F9VQ.png"
   }
 ]
 
